625f9f5b6a43bf829b442760ecbc76acc4254922
Clement Bouakkaz <clementbouakkaz@gmail.com>
2018-11-29 09:38:00 +0000

    implement new create and show user stories

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,21 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurant = Restaurant.all
+    @restaurants = Restaurant.all
+  end
+  def new
+    @restaurant = Restaurant.new
+  end
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
